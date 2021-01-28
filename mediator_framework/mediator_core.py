b9795c2aabdd846d0c4f88bdb9bff5d64befb71f
George <qiangzhang0925xx@gmail.com>
2021-01-28 15:46:51 +0800

    update mediator_core.py and mediator_controller.py

--- mediator_framework/mediator_core.py
+++ mediator_framework/mediator_core.py
@@ -1,4 +1,5 @@
 from mediator_framework.tp_list import *
+from mediator_framework.data_provider import *
 
 def locate_translation_point(opdata):
     namespace = opdata["data"]["xmlns"]
@@ -8,17 +9,44 @@ def locate_translation_point(opdata):
         return None
     return path,PyTranslateReg[0]
 
-def compute_configuration_by_neid(current_cc,opdata):
-    return None
+def compute_configuration_by_operation(neid, json_data):
+    path = json_data["path"]
+    current_cc = get_controller_configuration(neid, path)
+    cc = json_data["data"]
+    oper = json_data["op"]
+    expected_cc = None
+    if oper == "merge":
+        expected_cc = compute_merge(current_cc, cc)
+    elif oper == "create" or oper == "replace":
+        expected_cc  = cc
+    else:
+        expected_cc = compute_delete(current_cc,cc)
+    return expected_cc
 
-def translate_edit_congfig_content(json_data):
-    return None
+def translate_edit_congfig_content(data):
+    expected_dc = None
+    return expected_dc
 
-def translate_get_config_content(json_data):
-    return None
+def translate_get_config_content(data):
+    expected_dc = None
+    return expected_dc
 
 def translate_rpcreply_data(rpcreply_data):
     return None
 
-def compare_device_configuration(current_dc,converted_json_data):
+def compare_device_configuration(neid, expected_dc):
+    path = expected_dc["path"]
+    current_dc = get_device_configuration(neid,path)
+    msg = compare(expected_dc,current_dc)
+    return msg
+
+### compute operation
+def compute_merge(config, config1):
+    return None
+
+def compute_delete(config, config1):
+    return None
+
+### compare opeartion
+def compare(config,config1):
     return None
--- swagger_server/controllers/mediator_controller.py
+++ swagger_server/controllers/mediator_controller.py
@@ -2,6 +2,7 @@ import connexion
 import six
 
 from swagger_server import util
+from mediator_framework.mediator_core import *
 
 
 def translate_msg_from_adaptor(neid, msg_type, opdata):  # noqa: E501
@@ -18,4 +19,11 @@ def translate_msg_from_adaptor(neid, msg_type, opdata):  # noqa: E501
 
     :rtype: str
     """
-    return 'it works!'
+    converted_msg = None
+    if msg_type == "edit_config":
+        expected_cc = compute_configuration_by_operation(neid,opdata)
+        expected_dc = translate_edit_congfig_content(expected_cc)
+        converted_msg = compare_device_configuration(neid,expected_dc)
+    elif msg_type == "get_config":
+        converted_msg = translate_get_config_content(opdata)
+    return converted_msg
