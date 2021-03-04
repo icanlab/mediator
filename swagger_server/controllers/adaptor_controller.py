ceda72e1b6c5ceb9cb475037e5bcc30fcdeaa268
George <qiangzhang0925xx@gmail.com>
2021-03-04 17:14:38 +0800

    change the get to post

--- swagger_server/controllers/adaptor_controller.py
+++ swagger_server/controllers/adaptor_controller.py
@@ -6,8 +6,10 @@ import six
 # from swagger_server.controllers.mediator_controller import *
 from swagger_server.controllers.mediator_controller import translate_msg_from_adaptor
 from swagger_server.mediator_framework.adaptor  import *
+from swagger_server.models.input_msg import InputMsg
 
-def translate_msg(protocol, neid, xml_msg):  # noqa: E501
+
+def translate_msg(body=None):  # noqa: E501
     """translate msg
 
      # noqa: E501
@@ -21,12 +23,17 @@ def translate_msg(protocol, neid, xml_msg):  # noqa: E501
 
     :rtype: str
     """
-    if protocol == "netconf":
-        prepare = data_prepare(xml_msg)
-        header = prepare[0]
-        msg_config = prepare[1]
-        data_to_core = unlock(msg_config)
-        back = translate_msg_from_adaptor(neid, "edit_config", data_to_core)
-        data_to_plugin = package(header, back)
-        # data_to_plugin = data_to_plugin.replace("\n", "").replace("\t", "")
+    if connexion.request.is_json:
+        body = InputMsg.from_dict(connexion.request.get_json())  # noqa: E501
+        protocol = body.protocol
+        neid = body.neid
+        xml_msg = body.message
+        if protocol == "netconf":
+            prepare = data_prepare(xml_msg)
+            header = prepare[0]
+            msg_config = prepare[1]
+            data_to_core = unlock(msg_config)
+            back = translate_msg_from_adaptor(neid, "edit_config", data_to_core)
+            data_to_plugin = package(header, back)
+            # data_to_plugin = data_to_plugin.replace("\n", "").replace("\t", "")
     return data_to_plugin
