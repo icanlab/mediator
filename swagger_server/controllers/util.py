f033a058f7f2fa348dc2c90719f2433c4b627fe9
xymy <thyfan@163.com>
2021-03-09 14:07:57 +0800

    Do not convert for str, bytes and bytearray

--- swagger_server/controllers/util.py
+++ swagger_server/controllers/util.py
@@ -5,7 +5,7 @@ from lxml import etree
 
 
 def make_response_json(obj, status=200, headers=None):
-    if not isinstance(obj, str):
+    if not isinstance(obj, (str, bytes, bytearray)):
         response = json.dumps(obj)
     else:
         response = obj
@@ -15,9 +15,9 @@ def make_response_json(obj, status=200, headers=None):
 
 
 def make_response_xml(element_or_tree, status=200, headers=None):
-    if not isinstance(element_or_tree, str):
+    if not isinstance(element_or_tree, (str, bytes, bytearray)):
         response = etree.tostring(
-            element_or_tree, encoding="utf-8", xml_declaration=True, pretty_print=True
+            element_or_tree, encoding="utf-8", xml_declaration=True
         )
     else:
         response = element_or_tree
