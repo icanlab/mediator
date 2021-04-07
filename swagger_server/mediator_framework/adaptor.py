a2ebac7617e5dba50bf7b8f001c660c4e0284934
stars201014 <72848990+stars201014@users.noreply.github.com>
2021-04-07 13:59:55 +0800

    Update adaptor.py

--- swagger_server/mediator_framework/adaptor.py
+++ swagger_server/mediator_framework/adaptor.py
@@ -87,7 +87,7 @@ def get_child(content, attrib_op, ns_map):
     node['path'] = '/' + ns_list[get_ns(content)] + ':' + get_tag(content)
     if content[0].text is not None:     # add the namespace for path
         node['path'] = node['path'] + '[' + ns_list[get_ns(content[0])] + ':' + get_tag(content[0]) \
-                       + '=' + content[0].text + ']'
+                       + '="' + content[0].text + '"]'
     node['schema_path'] = ''
     node_list.append(node)
     if node['op'] != "delete" and node['op'] != "remove":
