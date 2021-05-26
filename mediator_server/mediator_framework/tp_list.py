c82bd3115a7e856fa4b93671a2e7b561a3641f7f
qiangzhang <qiangzhang0925@126.com>
2021-05-26 19:58:59 +0800

    Update tp_list.py

--- mediator_server/mediator_framework/tp_list.py
+++ mediator_server/mediator_framework/tp_list.py
@@ -27,10 +27,11 @@ translate_yang_registry = {('HUAWEI', 'ROUTER6500', 'HUAWEIOS', '1.0.1111.2'):
                                 '/ietf-interfaces:interfaces/interface': (_translate_ietf_interfaces_interface_obj, ietf_interfaces_binding, "yc_interfaces_ietf_interfaces__interfaces"),
                                 '/ietf-interfaces:interfaces/interface/ietf-ip:ipv4': (_translate_ietf_interfaces_interface_ipv4_obj, ietf_interfaces_binding, "yc_interface_ietf_interfaces__interfaces_interface"),
                                 # ietf-routing scripts
-                                # '/ietf-routing:routing': (_translate_ietf_routing_obj, ietf_routing_binding, "ietf_routing"),
-                                '/ietf-routing:routing': (_translate_ietf_routing_obj1, ietf_network_instance_schema_mount_binding, "ietf_routing"),
-                                # '/ietf-routing:routing/control-plane-protocols/control-plane-protocol': (_translate_ietf_routing_control_plane_protocol_obj, ietf_routing_binding, "yc_control_plane_protocols_ietf_routing__routing_control_plane_protocols"),
-                                '/ietf-routing:routing/control-plane-protocols/control-plane-protocol': (_translate_ietf_routing_control_plane_protocol_obj1, ietf_network_instance_schema_mount_binding, "yc_control_plane_protocols_ietf_routing__routing_control_plane_protocols"),
+                                # schema-mount script --> ietf
+                                # '/ietf-routing:routing': (_translate_ietf_routing_obj1, ietf_network_instance_schema_mount_binding, "ietf_routing"),
+                                # '/ietf-routing:routing/control-plane-protocols/control-plane-protocol': (_translate_ietf_routing_control_plane_protocol_obj1, ietf_network_instance_schema_mount_binding, "yc_control_plane_protocols_ietf_routing__routing_control_plane_protocols"),
+                                '/ietf-routing:routing': (_translate_ietf_routing_obj, ietf_routing_binding, "ietf_routing"),
+                                '/ietf-routing:routing/control-plane-protocols/control-plane-protocol': (_translate_ietf_routing_control_plane_protocol_obj, ietf_routing_binding, "yc_control_plane_protocols_ietf_routing__routing_control_plane_protocols"),
                                 '/ietf-routing:routing/control-plane-protocols/control-plane-protocol/ietf-bgp:bgp': (_translate_ietf_routing_bgp_obj, ietf_routing_binding, "yc_control_plane_protocol_ietf_routing__routing_control_plane_protocols_control_plane_protocol"),
                                 # ietf-l3vpn-ntw scripts
                                 '/ietf-l3vpn-ntw:l3vpn-ntw': (_translate_ietf_l3vpn_ntw_obj, ietf_l3vpn_ntw_binding, "ietf_l3vpn_ntw"),
@@ -44,9 +45,10 @@ translate_yang_registry = {('HUAWEI', 'ROUTER6500', 'HUAWEIOS', '1.0.1111.2'):
                                 '/huawei-bgp:bgp': (_translate_huawei_bgp_obj, huawei_bgp_binding, "huawei_bgp"),
                                 '/huawei-bgp:bgp1': (_translate_huawei_bgp_obj1, huawei_bgp_binding, "huawei_bgp"),
                                 # huawei-network-instance scripts
-                                # '/huawei-network-instance:network-instance': (_translate_huawei_network_instance_obj, huawei_bgp_binding, "huawei_network_instance"),
-                                # '/huawei-network-instance:network-instance1': (_translate_huawei_network_instance_obj1, huawei_bgp_binding, "huawei_network_instance")
-                                '/huawei-network-instance:network-instance': (_translate_huawei_network_instance_obj2, huawei_network_instance_sm_binding, "huawei_network_instance"),
+                                '/huawei-network-instance:network-instance': (_translate_huawei_network_instance_obj, huawei_bgp_binding, "huawei_network_instance"),
+                                '/huawei-network-instance:network-instance1': (_translate_huawei_network_instance_obj1, huawei_bgp_binding, "huawei_network_instance")
+                                # schema-mount script --> huawei
+                                # '/huawei-network-instance:network-instance': (_translate_huawei_network_instance_obj2, huawei_network_instance_sm_binding, "huawei_network_instance"),
                                }
                            }
 
